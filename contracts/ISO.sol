617da3f712a2f00e95af28fc05292de9f1619f25
BernardLowe <vitalikzhou@gmail.com>
2024-02-25 12:51:28 +0800

    iso

--- contracts/ISO.sol
+++ contracts/ISO.sol
@@ -7,25 +7,45 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ISO is Ownable {
     IERC20 public mamba;
+    IERC20 public toSale;
+    uint256 price; // mul 1000
+    uint256 total; // total mamba quota
+    uint256 left; // left mamba quota
+    uint256 begin;
+    uint256 end;
+    mapping(address => uint256) deposited;
+    mapping(address => bool) withdrawed;
 
-    struct TokenOffering {
-        address token;
-        uint256 price;          // mul 1000
-        uint256 total;          // total mamba quota
-        uint256 left;           // left mamba quota
-        mapping(address => uint256) deposited;
-    }
-
-    mapping(uint256 => TokenOffering) public offerings;
-
-    constructor(address _mamba) {
+    constructor(address _mamba, address _toSale, uint256 _begin, uint256 _end, uint256 _total, uint256 _price) {
         mamba = IERC20(_mamba);
+        toSale = IERC20(_toSale);
+        begin = _begin;
+        end = _end;
+        total = _total;
+        left = _total;
+        price = _price;
     }
 
-    function buy(uint256 offeringId, uint256 amount) external {
+    function buy(uint256 mambaAmount) external {
         address buyer = _msgSender();
-        TokenOffering storage offering = offerings[offeringId];
-        require(offering.left > amount);
-        require(mamba.transferFrom(buyer, address(this), amount));
+        require(block.timestamp >= begin && block.timestamp <= end, "MambaSwap ISO: closed");
+        require(left > mambaAmount, "MambaSwap ISO: no quota");
+        require(mamba.transferFrom(buyer, address(this), mambaAmount));
+        deposited[buyer] += mambaAmount;
+        left -= mambaAmount;
+    }
+
+    function withdraw() external {
+        address buyer = _msgSender();
+        require(withdrawed[buyer] == false, "MambaSwap ISO: withdrawd");
+        require(block.timestamp >= end, "MambaSwap ISO: not finished");
+        uint256 saledCount = count(buyer);
+        toSale.transfer(buyer, saledCount);
+        withdrawed[buyer] = true;
+    }
+
+    function count(address buyer) public view returns (uint256) {
+        uint256 mambaAmount = deposited[buyer];
+        return mambaAmount * 10000 / price;
     }
 }
